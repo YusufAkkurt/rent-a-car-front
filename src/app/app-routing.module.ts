57e9ea01e7ce75c66e45eb26d188845b86e65324
YusufAkkurt <ysfakkurt@hotmail.com>
2021-03-12 23:50:22 +0300

    Router mimarisi eklendi birkaç düzeltme yapıldı

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './components/customer/customer.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'rentals', component: RentalComponent },
+   { path: 'customers', component: CustomerComponent },
+   { path: 'cars', component: CarComponent },
+   { path: 'brands', component: BrandComponent },
+   { path: 'colors', component: ColorComponent },
+   { path: '**', redirectTo: 'rentals', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+   imports: [RouterModule.forRoot(routes)],
+   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ColorComponent } from './components/color/color.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CarComponent } from './components/car/car.component';
+import { RentalComponent } from './components/rental/rental.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    SidebarComponent,
+    CustomerComponent,
+    ColorComponent,
+    BrandComponent,
+    CarComponent,
+    RentalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
