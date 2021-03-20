66c9c4dce70bbf4ecc56c5370270bc6f236576d9
YusufAkkurt <ysfakkurt@hotmail.com>
2021-03-20 20:41:15 +0300

    Code refactoring

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CarService } from '../../services/car.service';
 import { CarDetail } from '../../models/carDetail';
 import { ActivatedRoute } from '@angular/router';
+import { BrandService } from '../../services/brand.service';
+import { ColorService } from '../../services/color.service';
+import { Brand } from '../../models/brand';
+import { Color } from '../../models/color';
 
 @Component({
    selector: 'app-car',
@@ -13,18 +17,39 @@ export class CarComponent implements OnInit {
 
    carDetail: CarDetail | undefined;
    carDetails: CarDetail[] = [];
+   brands: Brand[] = [];
+   colors: Color[] = [];
    filterText: string = '';
+   brandFilter: string = '';
+   colorFilter: string = '';
 
-   constructor(private carService: CarService, private activatedRoute: ActivatedRoute) {
+   constructor(private carService: CarService,
+               private brandService: BrandService,
+               private colorService: ColorService,
+               private activatedRoute: ActivatedRoute) {
    }
 
    ngOnInit(): void {
-      return this.getCars();
+      this.getCars();
+      this.getBrands();
+      this.getColors();
    }
 
    getCars() {
       this.carService.getCars().subscribe((response) => {
          this.carDetails = response.data;
       });
+   }
+
+   getBrands(){
+      this.brandService.getBrands().subscribe(response => {
+         this.brands = response.data
+      })
+   }
+
+   getColors(){
+      this.colorService.getColors().subscribe(response => {
+         this.colors = response.data
+      })
    }
 }
