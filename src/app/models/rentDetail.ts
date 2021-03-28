5c2089196eef4000936c916b9eb3d700a63a4bb6
YusufAkkurt <ysfakkurt@hotmail.com>
2021-03-28 18:18:36 +0300

    Code refactoring

---
+++ src/app/models/rentDetail.ts
@@ -0,0 +1,10 @@
+export interface RentDetail {
+   id: number
+   carBrand: string
+   carModel: string
+   customerFirstName: string
+   customerLastName: string
+   companyName: string
+   rentDate: Date
+   returnDate: Date
+}
